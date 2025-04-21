cce37da53617a39c9b1df63259990047a0274737
Crimcode <valentin.munteanu@tutanota.com>
2025-04-21 21:42:18 +0300

    feat: Add versioning
    Version bumped to 0.2.0

---
+++ scripts/setupGitHooks.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+# Run the script to link custom hooks to git
+
+GIT_HOOKS_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/../.git/hooks" && pwd)"
+CUSTOM_HOOKS_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/hooks" && pwd)"
+
+ln -s $CUSTOM_HOOKS_DIR/post-commit $GIT_HOOKS_DIR/post-commit
+chmod +x $GIT_HOOKS_DIR/post-commit
+
+ln -s $CUSTOM_HOOKS_DIR/prepare-commit-msg $GIT_HOOKS_DIR/prepare-commit-msg
+chmod +x $GIT_HOOKS_DIR/prepare-commit-msg
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,37 @@
+#include <print>
+#include <string>
+#include <vector>
+
+#ifndef TINYSQL_VERSION
+#define TINYSQL_VERSION "unknown"
+#endif
+
 extern int yyparse();
 
-int main() {
+namespace {
+  void help() {
+    std::println("Usage: tinysql [options]");
+    std::println(R"(Options:
+      -h, --help     -> Prints this menu 
+      -v, --version  -> Prints the current version)");
+  }
+}
+
+int main(int argc, char** argv) {
+  const std::vector<std::string> args{argv + 1, argc + argv};
+  for (const auto& arg : args) {
+    if (arg == "-h" || arg == "--help") {
+      help();
+      return 0;
+    }
+    if (arg == "-v" || arg == "--version") {
+      std::println("{}", TINYSQL_VERSION);
+      return 0;
+    }
+    std::println(stderr, "[ERROR] Unknown option '{}' !", arg);
+    help();
+    return 1;
+  }
+
   yyparse();
 }
