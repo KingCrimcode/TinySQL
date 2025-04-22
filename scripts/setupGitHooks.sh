1d834765690a114edac353178323e61c43f6a2a4
Crimcode <valentin.munteanu@tutanota.com>
2025-04-22 15:04:59 +0300

    fix: Commit version change properly
    Now commits version changes AFTER major/minor fixes or patches.
    Creates tag on version change.

--- scripts/setupGitHooks.sh
+++ scripts/setupGitHooks.sh
@@ -7,6 +7,3 @@ CUSTOM_HOOKS_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/hooks" && pwd)"
 
 ln -s $CUSTOM_HOOKS_DIR/post-commit $GIT_HOOKS_DIR/post-commit
 chmod +x $GIT_HOOKS_DIR/post-commit
-
-ln -s $CUSTOM_HOOKS_DIR/prepare-commit-msg $GIT_HOOKS_DIR/prepare-commit-msg
-chmod +x $GIT_HOOKS_DIR/prepare-commit-msg
